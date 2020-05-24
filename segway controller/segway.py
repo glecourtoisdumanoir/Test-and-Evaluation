4bec230f2e9c838b83ebfacf109440652635e59a
g.lecourtoisdumanoir <g.lecourtoisdumanoir@gmail.com>
2020-05-23 19:57:48 -0700

    extended the path taken by the moving obstacle

--- segway controller/segway.py
+++ segway controller/segway.py
@@ -2,7 +2,7 @@ from tulip import transys
 from tulip import spec
 from tulip import synth
 from tulip import dumpsmach
-import logging
+from tulip.transys import machines
 # 
 # The system is modeled as a discrete transition system.
 # The Segway can be located anyplace on a 4x2 grid of cells. 
@@ -13,8 +13,8 @@ import logging
 # indicates that the quadrotor should move to cell X7.
 #    --------------
 #    -- X0 -- X4 --     Segway: X0 
-#    -- X1 -- X5 --     Egg: X7
-#    -- X2 -- X6 --     1 turtle robot (moving obstacle): X1-X2
+#    -- X1 -- X5 --     Egg: X7 || X3 || X6
+#    -- X2 -- X6 --     1 turtle robot (moving obstacle): X1-X2-X6-X5-X1...
 #    -- X3 -- X7 --
 #    --------------
 
@@ -28,13 +28,21 @@ import logging
 # the searchTheEgg signal is turned on infinitely often.
 env_vars = {}
 env_vars['moveToTheEgg'] = 'boolean' #corresponds to the quadrotor is landed on the segway and eggCoordinates has been received
-env_vars['turtleRobotLocation'] = (1, 2)
+env_vars['turtleRobotLocation'] = (1, 6)
+env_vars['eggLocation'] = (3, 7) #can take values 3 or 6 or 7
 env_init = set() #env_vars initialized in the simulate.py document
 env_prog = {'moveToTheEgg'} #[]<>
 env_safe = set()
-env_safe = {
-            '(turtleRobotLocation = 1) -> X (turtleRobotLocation = 2 || turtleRobotLocation = 1)',
-            '(turtleRobotLocation = 2) -> X (turtleRobotLocation = 2 || turtleRobotLocation = 1)',
+env_safe = { #moving obstacle
+            '(turtleRobotLocation != 3)',
+            '(turtleRobotLocation != 4)',
+            '(turtleRobotLocation = 1) -> X (turtleRobotLocation = 5 || turtleRobotLocation = 2)',
+            '(turtleRobotLocation = 2) -> X (turtleRobotLocation = 1 || turtleRobotLocation = 6)',
+            '(turtleRobotLocation = 5) -> X (turtleRobotLocation = 1 || turtleRobotLocation = 6)',
+            '(turtleRobotLocation = 6) -> X (turtleRobotLocation = 2 || turtleRobotLocation = 5)',
+            '(eggLocation != 4)',
+            '(eggLocation != 5)',
+            '(X (eggLocation) = eggLocation)',
            } 
 
 # System dynamics
@@ -43,7 +51,7 @@ env_safe = {
 # action.
 sys_vars = {}
 sys_vars['segwayLocation'] = (0, 7)
-sys_vars['X7reach'] = 'boolean'
+sys_vars['XeggReach'] = 'boolean'
 sys_vars['batteryLevel'] = (0, 15)
 sys_safe = {
             '(segwayLocation = 0) -> X (segwayLocation = 0 || segwayLocation = 4 || segwayLocation = 1)',
@@ -61,6 +69,8 @@ sys_safe = {
             
             '(turtleRobotLocation = 1) -> !(segwayLocation=1)', 
             '(turtleRobotLocation = 2) -> !(segwayLocation=2)',
+            '(turtleRobotLocation = 5) -> !(segwayLocation=5)',
+            '(turtleRobotLocation = 6) -> !(segwayLocation=6)',
             }
 sys_prog = set()               
 
@@ -68,23 +78,23 @@ sys_prog = set()
 #
 # The system specification is that the segway should repeatedly revisit
 # the upper left corner of the grid while at the same time responding
-# to the moveToTheEgg signal by visiting the lower right corner.  The LTL
+# to the moveToTheEgg signal by visiting the eggLocation.  The LTL
 # specification is given by
 #
-#     []<> X0 && [](moveToTheEgg -> <>X7) && [](!moveToTheEgg -> <>X0)
+#     []<> X0 && [](moveToTheEgg -> <>eggLocation) && [](!moveToTheEgg -> <>X0)
 #
-# Since this specification is not in GR(1) form, we introduce three
-# environment variables X7reach X6reach X3 reach that are initialized to True and the specification :
-#      [](moveToTheEgg -> <>X7)
+# Since this specification is not in GR(1) form, we introduce one
+# environment variable XeggReach that is initialized to True and the specification :
+#      [](moveToTheEgg -> <>eggLocation)
 # becomes :
-#      [](X (X7reach) <-> (X7)) || (X7reach && !moveToTheEgg), 
+#      [](X (XeggReach) <-> (eggLocation)) || (XeggReach && !moveToTheEgg), 
 #    
 # Augment the system description to make it GR(1)
-sys_init = {'X7reach', 'batteryLevel = 15', 'segwayLocation = 0'} #does not work with 2x sys_init ={...}
+sys_init = {'XeggReach', 'batteryLevel = 1', 'segwayLocation = 0'} #does not work with 2x sys_init ={...}
 sys_safe |= {
-            '(X (X7reach) <-> (segwayLocation=7)) || (X7reach && !moveToTheEgg)',
+            '(X (XeggReach) <-> (segwayLocation=eggLocation)) || (XeggReach && !moveToTheEgg)',
             }
-sys_prog |= {'X7reach', 'segwayLocation = 0'}
+sys_prog |= {'XeggReach', 'segwayLocation = 0'}
 
 # Create a GR(1) specification
 specs = spec.GRSpec(env_vars, sys_vars, env_init, sys_init, env_safe, sys_safe, env_prog, sys_prog)
@@ -93,7 +103,8 @@ specs.moore = True
 specs.qinit = '\E \A'
 
 # Controller synthesis
-ctrl = synth.synthesize('omega', specs)
-assert ctrl is not None, 'unrealizable'
+strategy = synth.synthesize('omega', specs)
+assert strategy is not None, 'unrealizable'
 
-dumpsmach.write_python_case("segwayController.py", ctrl, classname="controller")
+dumpsmach.write_python_case("segwayController.py", strategy, classname="controller")
+machines.random_run(strategy, N=100)
--- segway controller/simulate.py
+++
@@ -1,8 +0,0 @@
-from __future__ import print_function
-from segwayController import controller
-
-M = controller()
-for i in xrange(100):
-    input_values = {"moveToTheEgg": False, "turtleRobotLocation":2}
-    print(M.move(**input_values))
-
